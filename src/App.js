3023bd569751b5b9417564e0e78949b0a2698aa9
aaveggupta <aaveggupta.contact@gmail.com>
2023-02-01 10:19:56 +0530

    poc basic setup added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, useState } from "react";
+import "./App.css";
+import i18n from "i18next";
+import { useTranslation, initReactI18next, Trans } from "react-i18next";
+import HttpBackend from "i18next-http-backend";
 
-function App() {
+i18n
+  .use(initReactI18next)
+  .use(HttpBackend)
+  .init({
+    backend: { loadPath: "/translations/{{lng}}.json" },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  });
+
+const App = () => {
+  const { t } = useTranslation();
+  const [count, setCount] = useState(0);
+  const onChange = (event) => {
+    i18n.changeLanguage(event.target.value);
+    setCount((previousCount) => previousCount + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={"Loading..."}>
+      <div className="App">
+        <header className="App-header">
+          <h1>{t("welcome")}</h1>
+          <p>
+            <Trans components={{ bold: <strong />, italic: <i /> }}>
+              sampleText
+            </Trans>
+          </p>
+          <p>{t("changed", { count })}</p>
+          <select name="language" onChange={onChange}>
+            <option value="en">English</option>
+            <option value="id">Indonesian</option>
+          </select>
+        </header>
+      </div>
+    </Suspense>
   );
-}
+};
 
 export default App;
